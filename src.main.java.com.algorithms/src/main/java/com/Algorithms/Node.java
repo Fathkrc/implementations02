6e093c5c9414f80e5846141815b816b8090bcc6f
Fatih Karaca <karacaafatih@gmail.com>
2025-09-02 22:40:47 +0300

    changed the structure fits for maven

---
+++ src.main.java.com.algorithms/src/main/java/com/Algorithms/Node.java
@@ -0,0 +1,14 @@
+package src.main.java.com.Algorithms;
+
+public class Node<T> {
+
+    T data;
+    Node<T> next;
+
+    // Generic data
+    public Node(T data) {
+        this.data = data;
+        this.next=null;
+    }
+
+}
