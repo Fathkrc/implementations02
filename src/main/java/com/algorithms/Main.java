b059f3af00a53a4f03f082df6bf0ed9b92b93ebf
Fatih Karaca <karacaafatih@gmail.com>
2025-09-04 20:15:27 +0300

    added DynamicIntArray test cases

--- src/main/java/com/algorithms/Main.java
+++ src/main/java/com/algorithms/Main.java
@@ -3,25 +3,25 @@ package com.algorithms;
 
 public class Main {
     public static void main(String[] args) {
-        com.algorithms.DynamicIntArray test1 = new com.algorithms.DynamicIntArray();
-        test1.add(12);
-        test1.add(25);
-        test1.add(45);
-        System.out.println(test1.get(0));
+//      DynamicIntArray test1 = new DynamicIntArray();
+//        test1.add(12);
+//        test1.add(25);
+//        test1.add(45);
+//        System.out.println(test1.get(0));
 //        System.out.println("Capacity of Dynamic Int Array is " + test1.arrayCapacity());// capacity 4
-        test1.add(66);
-        test1.add(100);
-        test1.add(33);
-        test1.add(222);
-        test1.add(555);
-        test1.add(444);
-        for (int i = 0; i < test1.size(); i++) {
-            System.out.println(test1.get(i));
-        }
-        System.out.println("Dynamic Int array has " + test1.size() + " items");
-        System.out.println("Capacity of Dynamic Int Array is " + test1.arrayCapacity());// 16
-        System.out.println("Is Array Empty ? : " + test1.isEmpty());
-//        try {
+//        test1.add(66);
+//        test1.add(100);
+//        test1.add(33);
+//        test1.add(222);
+//        test1.add(555);
+//        test1.add(444);
+//        for (int i = 0; i < test1.size(); i++) {
+//            System.out.println(test1.get(i));
+//        }
+//        System.out.println("Dynamic Int array has " + test1.size() + " items");
+//        System.out.println("Capacity of Dynamic Int Array is " + test1.arrayCapacity());// 16
+//        System.out.println("Is Array Empty ? : " + test1.isEmpty());
+////        try {
 //            System.out.println("index out of bound ?  : "+ test1.get(test1.arrayCapacity()));
 //        } catch (Exception e) {
 //            System.out.println(e.getMessage()+ " *** Error Case ***");
---
+++ src/test/java/com/Algorithms/DynamicIntArrayTest.java
@@ -0,0 +1,88 @@
+package test.java.com.Algorithms;
+
+import com.algorithms.DynamicIntArray;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DynamicIntArrayTest {
+    private DynamicIntArray array;
+
+    @BeforeEach
+    void init() {
+        array = new DynamicIntArray();
+    }
+
+    @Test
+    void newCreatedArray_isSize0_isCapacity2() {
+        assertTrue(array.isEmpty());
+        assertEquals(0, array.size());
+        assertEquals(2, array.arrayCapacity());
+    }
+
+
+    @Test
+    void isAddingValueIncreasingArraysSize() {
+        for (int i = 0; i < 6; i++) {
+            array.add(i);
+            if (i == 2) assertEquals(4, array.arrayCapacity());
+            if (i == 5) assertEquals(8, array.arrayCapacity());
+        }
+    }
+
+    @Test
+    void DoesAddToIndexAddingToExactIndex() {
+        array.add(1);
+        array.add(3);
+        array.add(1, 2);
+        assertEquals(1, array.get(0));
+        assertEquals(2, array.get(1));
+        assertEquals(3, array.get(2));
+    }
+
+    @Test
+    void DoesAddItem_outOfBounds_throwsIndexOutOfBoundException() {
+        array.add(111);
+        assertThrows(IndexOutOfBoundsException.class, () -> array.add(-1, 333));
+        assertThrows(IndexOutOfBoundsException.class, () -> array.add(10, 666));
+    }
+
+    @Test
+    void getItemOutOfBounds_ThrowsIndexOutOfBoundsException() {
+        array.add(111);
+        assertEquals(111, array.get(0));
+        assertThrows(IndexOutOfBoundsException.class, () -> array.get(-1));
+        assertThrows(IndexOutOfBoundsException.class, () -> array.get(2));
+    }
+
+    @Test
+    void RemoveExistingFirstItemThatGiven() {
+        array.add(111);
+        array.add(222);
+        array.add(333);
+        array.add(444);
+        array.add(111);
+        boolean removedOrNot = array.remove(111);
+        assertTrue(removedOrNot); // removed first occurrence
+        assertEquals(222, array.get(0));
+        assertEquals(111, array.get(3)); // 222, 333, 444, 111
+        assertEquals(4, array.size());// size must be decreased
+    }
+
+    @Test
+    void iterator_iteratesInOrder_andThrowsAfterEnd() {
+        for (int i = 0; i < 5; i++) array.add(i);
+
+        var testIterator = array.iterator();
+        for (int i = 0; i < 5; i++) {
+            assertTrue(testIterator.hasNext());
+            assertEquals(i, array.get(i)); // not sure here
+            testIterator.next();
+        }
+        assertFalse(testIterator.hasNext());
+        assertThrows(NoSuchElementException.class, testIterator::next);
+    }
+}
